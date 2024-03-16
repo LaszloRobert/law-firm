b122139776898d9d9be35713b44c0e549d357658
Laszlo Robert <robertolaszlo@yahoo.com>
2024-03-16 14:02:54 +0200

    next iteration

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -13,7 +13,7 @@ const SectionWrapper = (Component, idName) =>
                 variants={staggerContainer()}
                 initial="hidden"
                 whileInView="show"
-                viewport={{ once: true, amount: 0.25 }}
+                viewport={{ once: true, amount: 0.1 }}
                 className={`${style.padding} relative`}
             >
                 <span className='hash-span' id={idName}>
